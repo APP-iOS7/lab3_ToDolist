df608fc4b86026ef6cf8d012b8832775e27bade2
aCafela-coffee <acafelacoffee@gmail.com>
2024-12-27 11:24:28 +0900

    삭제 버튼 만들기 함수 추가

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -2,14 +2,28 @@ function addTodo(text, checked = false) {
 
 }
 
+function makeDeleteButton(li) {
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("btn", "btn-danger", "btn-sm", "ms-2");
+  deleteButton.textContent = "삭제";
+  deleteButton.addEventListener("click", () => {
+    const todos = loadTodos();
+    const index = Array.from(li.parentElement.children).indexOf(li);
+    todos.splice(index, 1);
+    saveTodos(todos);
+    li.remove();
+  });
+  return deleteButton;
+}
+
 function loadTodos() {
-    const savedTodos = localStorage.getItem('todoList');
-    return savedTodos ? JSON.parse(savedTodos) : [];
+  const savedTodos = localStorage.getItem('todoList');
+  return savedTodos ? JSON.parse(savedTodos) : [];
 }
 
 function initialize() {
-    const todos = loadTodos();
-    todos.forEach(todo => {
+  const todos = loadTodos();
+  todos.forEach(todo => {
     addTodo(todo.text, todo.checked);
   });
 }
