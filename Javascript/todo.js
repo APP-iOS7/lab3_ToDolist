92e173beba6280274f506ab87a08c570fd936ebc
Youngmin Cho <choyoungmin0322@gmail.com>
2024-12-27 11:34:19 +0900

    feature/Create

--- Javascript/todo.js
+++ Javascript/todo.js
@@ -1,18 +1,44 @@
-function addTodo(text, checked = false) {
+const todoListElement = document.getElementById('todoList');
+const addButton = document.getElementById('addTodo');
+const todoInput = document.getElementById('todoInput');
 
+function addTodo(text, checked = false) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  li.classList.add('list-group-item');
+  
+  todoListElement.appendChild(li);
+}
+
+function saveTodos() {
+  const todos = Array.from(todoListElement.children).map(li => ({
+      text: li.textContent.trim(),
+    }));
+  localStorage.setItem('todoList', JSON.stringify(todos));
 }
 
 function loadTodos() {
-    const savedTodos = localStorage.getItem('todoList');
-    return savedTodos ? JSON.parse(savedTodos) : [];
+  const savedTodos = localStorage.getItem('todoList');
+  return savedTodos ? JSON.parse(savedTodos) : [];
 }
 
 function initialize() {
-    const todos = loadTodos();
-    todos.forEach(todo => {
+  const todos = loadTodos();
+  todos.forEach(todo => {
     addTodo(todo.text, todo.checked);
   });
-}
 
+  addButton.addEventListener('click', () => {
+    if (todoInput.value.trim() === '') return;
+
+    addTodo(todoInput.value);
+
+    const todos = loadTodos();
+    todos.push({ text: todoInput.value, checked: false });
+    saveTodos(todos);
+    
+    todoInput.value = ''; 
+  });
+}
 
 document.addEventListener('DOMContentLoaded', initialize);
